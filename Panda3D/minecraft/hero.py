94bc10a81842094229ae154d8e4279b7dedf0ddb
a4six <beverly_csu@mail.ru>
2022-11-15 20:06:58 +0500

    Update hero.py

--- Panda3D/minecraft/hero.py
+++ Panda3D/minecraft/hero.py
@@ -13,10 +13,12 @@ KEY_TURN_RIGHT = 'm'
 class Hero:
     def __init__(self, position, land):
         self.land = land
+        self.mode = True
         self.hero = loader.loadModel('smiley')
         self.hero.setColor(1, 0.5, 0)
         self.hero.setScale(0.3)
         self.hero.reparentTo(render)
+        self.hero.setPos(position)
         self.cameraBind()
         self.accept_events()
 
@@ -39,6 +41,48 @@ class Hero:
             self.cameraUp()
         else:
             self.cameraBind()
+
+    def turn_left(self):
+        self.hero.setH((self.hero.getH() + 5) % 360)
+
+    def turn_right(self):
+        self.hero.setH((self.hero.getH() - 5) % 360)
+
+    def check_dir(self, angle):
+        if angle >= 0 and angle <= 20:
+            return (0, -1)
+        elif angle <= 65:
+            return (1, -1)
+        elif angle <= 110:
+            return (1, 0)
+        elif angle <= 155:
+            return (1, 1)
+        elif angle <= 200:
+            return (0, 1)
+        elif angle <= 245:
+            return (-1, 1)
+        elif angle <= 290:
+            return (-1, 0)
+        elif angle <= 335:
+            return (-1, -1)
+        else:
+            return (0, -1)
+
+    def look_at(self, angle):
+        x_from = round(self.hero.getX())
+        y_from = round(self.hero.getY())
+        z_from = round(self.hero.getZ())
+
+        dx, dy = self.check_dir(angle)
+        x_to = x_from + dx
+        y_to = y_from + dy
+        return (x_to, y_to, z_from)
+
+    def just_move(self, angle):
+        pos = self.look_at(angle)
+        self.hero.setPos(pos)
+
+    
 
     def accept_events(self):
         base.accept(KEY_SWITCH_CAMERA, self.changeView)
