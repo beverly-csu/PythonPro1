1cae0ee97d41900a12eac3417041b6211745d255
a4six <beverly_csu@mail.ru>
2023-01-03 20:55:34 +0500

    s

--- DataBase/Create and fill/create.py
+++ DataBase/Create and fill/create.py
@@ -22,4 +22,29 @@ def create():
     open()
     cursor.execute('''PRAGMA foreign_keys=on''')
 
+    do('''CREATE TABLE IF NOT EXISTS quiz (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR)''')
+
+    do('''CREATE TABLE IF NOT EXISTS question (
+        id INTEGER PRIMARY KEY,
+        question VARCHAR,
+        answer VARCHAR,
+        wrong1 VARCHAR,
+        wrong2 VARCHAR,
+        wrong3 VARCHAR)''')
+
+    do('''CREATE TABLE IF NOT EXISTS quiz_content (
+        id INTEGER PRIMARY KEY,
+        quiz_id INTEGER,
+        question_id INTEGER,
+        FOREIGN KEY (quiz_id) REFERENCES quiz (id),
+        FOREIGN KEY (question_id) REFERENCES question (id))''')
     
+    close()
+
+def main():
+    create()
+
+if __name__ == '__main__':
+    main()
