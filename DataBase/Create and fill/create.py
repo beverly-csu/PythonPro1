fb45bcda5a3708e304c07da9bd7c2b541d4c9f0c
a4six <beverly_csu@mail.ru>
2023-01-10 20:33:04 +0500

    Update create.py

--- DataBase/Create and fill/create.py
+++ DataBase/Create and fill/create.py
@@ -43,9 +43,59 @@ def create():
     
     close()
 
+def add_questions():
+    questions = [
+        ('Сколько месяцев в году имеют 28 дней?', 'Все', 'Один', 'Ни одного', 'Два'),
+        ('Каким станет зелёный утёс, если упадёт в Красное море?', 'Мокрым', 'Красным', 'Не изменится', 'Фиолетовым'),
+        ('Какой рукой лучше размешивать чай?', 'Ложкой', 'Правой', 'Левой', 'Любой'),
+        ('Что не имеет длины, глубины, ширины, высоты, а можно измерить?', 'Время', 'Глупость', 'Море', 'Воздух'),
+        ('Когда сетью можно вытянуть воду?', 'Когда вода замерзла', 'Когда нет рыбы', 'Когда уплыла золотая рыбка', 'Когда сеть порвалась'),
+        ('Что больше слона и ничего не весит?', 'Тень слона', 'Воздушный шар', 'Парашют', 'Облако')
+    ]
+    open()
+    cursor.executemany('''INSERT INTO question (question, answer, wrong1, wrong2, wrong3) VALUES (?, ?, ?, ?, ?)''', questions)
+    conn.commit()
+    close()
+
+def add_quiz():
+    quizes = [
+        ('Своя игра', ),
+        ('Кто хочет стать миллионером?', ),
+        ('Самый умный', )
+    ]
+    open()
+    cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quizes)
+    conn.commit()
+    close()
+
+def add_links():
+    open()
+    cursor.execute('''PRAGMA foreign_keys=on''')
+    query = '''INSERT INTO quiz_content (quiz_id, question_id) VALUES (?, ?)'''
+    answer = input('Добавить связь (y / n)?')
+    while answer != 'n':
+        quiz_id = int(input('id викторины: '))
+        question_id = int(input('id вопроса: '))
+        cursor.execute(query, (quiz_id, question_id))
+        conn.commit()
+        answer = input('Добавить связь (y / n)?')
+    close()
+
+def show(table):
+    query = '''SELECT * FROM ''' + table
+    open()
+    cursor.execute(query)
+    print(cursor.fetchall())
+    close()
+
+def show_tables():
+    show('question')
+    show('quiz')
+    show('quiz_content')
 
 def main():
     create()
+    show_tables()
 
 if __name__ == '__main__':
     main()
