d7ba183ae6dca9412fd944257dca83d3cb0352ac
a4six <beverly_csu@mail.ru>
2023-01-24 15:29:11 +0500

    Update quiz.py

--- DataBase/Create and fill/quiz.py
+++ DataBase/Create and fill/quiz.py
@@ -2,24 +2,20 @@ from random import randint
 from flask import Flask, session, redirect, url_for
 from db_scripts import get_question_after
 
-quiz = 0
-last_question = 0
 
 def index():
-    global quiz, last_question
     max_quiz = 3
-    quiz = randint(1, max_quiz)
-    last_question = 0
+    session['quiz'] = randint(1, max_quiz)
+    session['last_question'] = 0
     return '<a href="/test">Начать викторину</a>'
 
 def test():
-    global last_question
-    result = get_question_after(last_question, quiz)
+    result = get_question_after(session['last_question'], session['quiz'])
     if result is None or len(result) == 0:
         return redirect(url_for('result'))
     
     else:
-        last_question = result[0]
+        session['last_question'] = result[0]
         return '<p>' + str(result) + '</p>'
 
 def result():
@@ -29,6 +25,7 @@ app = Flask(__name__)
 app.add_url_rule('/', 'index', index)
 app.add_url_rule('/test', 'test', test)
 app.add_url_rule('/result', 'result', result)
+app.config['SECRET_KEY'] = 'sadsadsadsasadsa'
 
 if __name__ == '__main__':
     app.run()
