a27742fce8570d9af7e777f0cbf0fdb84bf717a5
a4six <beverly_csu@mail.ru>
2023-02-14 16:13:36 +0500

    update

--- DataBase/Create and fill/quiz.py
+++ DataBase/Create and fill/quiz.py
@@ -1,5 +1,5 @@
 from random import randint
-from flask import Flask, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
 from db_scripts import get_question_after, get_quizes, check_answer
 
 def save_answers():
@@ -10,21 +10,10 @@ def save_answers():
     if check_answer(quest_id, answer):
         session['answers'] += 1
 
-def quizes_list():
-    html = '''<html><head><title>Выбор викторины</title></head>
-    <body><h2>Выберите викторину:</h2><form method="POST" action="/">
-    <select name="quiz">'''
-    for quiz in get_quizes():
-        html += f'''<option value="{quiz[0]}">{quiz[1]}</option>'''
-    html += '''</select><p><input type="submit" value="Выбрать"></p>
-    </form></body></html>'''
-    return html
-
 def index():
     if request.method == 'GET':
         session['last_question'] = 0
-        quiz_form = quizes_list()
-        return quiz_form
+        return render_template('start.html', q_list=get_quizes())
     else:
         quiz_id = request.form.get('quiz')
         session['quiz'] = int(quiz_id)
@@ -35,19 +24,26 @@ def test():
         return redirect(url_for('index'))
 
     result = get_question_after(session['last_question'], session['quiz'])
+    if request.method == 'POST':
+        save_answers()
+
     if result is None or len(result) == 0:
         return redirect(url_for('result'))
     
     else:
         session['last_question'] = result[0]
-        return '<p>' + str(result) + '</p>'
+        answers_list = [result[2], result[3], result[4], result[5]]
+        from random import shuffle
+        shuffle(answers_list)
+        return render_template('test.html', quest_id=result[0], question=result[1], answers_list=answers_list)
 
 def result():
     return '<h3>Thats all :)</h3>'
 
-app = Flask(__name__)
+import os
+app = Flask(__name__, template_folder=os.getcwd(), static_folder=os.getcwd())
 app.add_url_rule('/', 'index', index, methods=['GET', 'POST'])
-app.add_url_rule('/test', 'test', test)
+app.add_url_rule('/test', 'test', test, methods=['GET', 'POST'])
 app.add_url_rule('/result', 'result', result)
 app.config['SECRET_KEY'] = 'sadsadsadsasadsa'
 
